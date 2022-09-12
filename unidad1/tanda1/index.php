69f45ccfe274e188a0d110486c58751caa550882
Elena2332 <elenafesional@gmail.com>
2022-09-12 09:50:52 +0200

    inicio
    creacion de ficheros

---
+++ unidad1/tanda1/index.php
@@ -0,0 +1,24 @@
+<?php 
+    echo "<script>console.log('Hola');</script>";
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+    <ul>
+        <li><a href="01.php">ejercicio 1</a></li>
+        <li><a href="02.php">ejercicio 2</a></li>
+        <li><a href="03.php">ejercicio 3</a></li>
+        <li><a href="04.php">ejercicio 4</a></li>
+        <li><a href="05.php">ejercicio 5</a></li>
+        <li><a href="06.php">ejercicio 6</a></li>
+        <li><a href="07.php">ejercicio 7</a></li>
+    </ul>
+</body>
+</html>
