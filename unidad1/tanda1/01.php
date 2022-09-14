7a615846645f7916ef787aca4cc6c887502ec188
Elena2332 <elenafesional@gmail.com>
2022-09-14 14:28:00 +0200

    arreglar punto 2

--- unidad1/tanda1/01.php
+++ unidad1/tanda1/01.php
@@ -9,12 +9,15 @@
 <body>
     <h1>punto 1</h1>
     <?php
+        
         echo date("d\\t\h M Y, l");
-
     ?>
     <h1>dias para que termmine el año</h1>
     <?php 
-    
+        $ahora = time(); // ahora en segundos
+        $maxSeg = 325*24*3600; // segundos en un año (dias * horas en un dia * segundos en una hora)
+        $dias = ($maxSeg-$ahora) * 3600*24; // 
+        echo $dias;
     ?>
 </body>
 </html>
