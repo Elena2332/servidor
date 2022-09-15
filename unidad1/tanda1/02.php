3f8571d16e1ddb25eb4b9950f26e84d97604d064
Elena2332 <elenafesional@gmail.com>
2022-09-15 21:16:56 +0200

    ejercicio2 comienzos

---
+++ unidad1/tanda1/02.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio2</title>
+</head>
+<body>
+    <h2>Array con la temperatura de varios dias</h2>
+    <div>
+        <?php
+            $arr=[];
+        ?>
+        <br>
+    </div>
+</body>
+</html>
