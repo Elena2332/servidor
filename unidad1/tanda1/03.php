f78196afae4379742cbd8700510f7e7f4225af20
Elena2332 <elenafesional@gmail.com>
2022-09-19 09:39:08 +0200

    hasta el 5 terminado

--- unidad1/tanda1/03.php
+++ unidad1/tanda1/03.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicio2</title>
+    <title>Ejercicio3</title>
 </head>
 <body>
-    <h2>Array con la temperatura de varios dias</h2>
     <div>
+        <h2>Ciudades sin repetir</h2>
         <?php
             $arr=['Burgos','Miranda','Vitoria','Burgos','Aranda','Vitoria'];
             $sinRepes = array_unique($arr);
