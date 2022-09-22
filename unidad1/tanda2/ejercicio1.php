78df723ea357d4f09c18771af0e1ca2254ecde3e
Elena2332 <elenafesional@gmail.com>
2022-09-22 14:05:42 +0200

    comienzo ejercicio1

--- unidad1/tanda2/ejercicio1.php
+++ unidad1/tanda2/ejercicio1.php
@@ -7,6 +7,25 @@
     <title>Ejercicio1</title>
 </head>
 <body>
+    <form>
+        <div>
+            <label>Texto a cifrar</label>
+            <input id="inpTexto" type="text"/>
+        </div>
+        <div>
+            <label>Desplazamiento:</label>
+            <input type="radio" name="numDes"/>
+            <input type="radio" name="numDes"/>
+            <input type="radio" name="numDes"/>
+            <button type="button" id="btnRad">Cifrado Cesar</button>
+        </div>
+        <div>
+            <label>Fichero de clave:</label>
+            <input id="inpFich" type="file"/>
+            <button type="button" id="btnFich"></button>
+        </div>
+    </form>
+
     <?php
 
     ?>
