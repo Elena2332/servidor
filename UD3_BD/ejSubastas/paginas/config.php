ad70f45a745d5e1e83752b837a4910b4a4e722e9
Elena2332 <elenafesional@gmail.com>
2022-10-25 12:27:22 +0200

    /

--- UD3_BD/ejSubastas/paginas/config.php
+++ UD3_BD/ejSubastas/paginas/config.php
@@ -6,10 +6,46 @@
     const DB_DATABASE="ud3";
 
     const RUTA_APP="./";
-    const RUTA_IMG="./img/";
+    const RUTA_IMG="../img/";
 
     const TITULO="El Mundo Bajo la Mesa";
     const MONEDA="€";
 
     session_start();
+
+
+    function obtenerItem($conn,$id_cat)
+    {
+        $str_sql = "select * from items where id_cat=$id_cat"; 
+        $items = mysqli_query($conn,$str_sql);
+        return $items;   
+    }
+
+    function pujasPorItem($conn,$idItem)
+    {
+        $sql = "select count(id) from pujas where id_item=$idItem";
+        $count = mysqli_fetch_assoc(mysqli_query($conn,$sql));
+        return $count;
+    }
+
+    function pujaMayor($conn,$idItem)
+    {
+        $sql = "select Max(cantidad) from pujas where id_item=$idItem";
+        $max = mysqli_fetch_assoc(mysqli_query($conn,$sql));
+        return $max;
+    }
+
+    function obtenerImagen($conn,$idItem)
+    {
+        $sql = "select imagen from imagenes where id_item=$idItem";
+        $res = mysqli_query($conn,$sql);
+        $imgs = mysqli_fetch_array($res);
+        if(empty($imgs[0]))
+            return 'NO IMAGEN';
+        else
+            return '<img src="'.RUTA_IMG.$imgs[0].'" alt="'.$imgs[0].'"/>';
+    }
+
+
+
 
--- UD3_BD/ejSubastas/paginas/listProductos.php
+++ UD3_BD/ejSubastas/paginas/listProductos.php
@@ -1,11 +1,22 @@
 <?php
     // TABLA PRODUCTOS
-    $str_where = ";";
+    $id_cat;
     if(isset($_GET['id']))
-    {
-        $str_where = " where ";
-    }
-    $sql = "";
+        $id_cat = $_GET['id'];
+    else
+        $id_cat = "true";
 
+    echo '<table>';
+    while($item = mysqli_fetch_assoc(obtenerItem($con,$id_cat)))
+    { 
+        echo "<tr>
+            <td>".obtenerImagen($con,$item['id'])."</td>
+            <td>".$item['nombre']."</td>
+            <td>".pujasPorItem($con,$item['id'])."</td>
+            <td>".pujaMayor($con,$item['id'])."</td>
+            <td>".$item['fechafin']."</td>
+        </tr>";
+    }    
 
+    echo '</table>';
 ?>
