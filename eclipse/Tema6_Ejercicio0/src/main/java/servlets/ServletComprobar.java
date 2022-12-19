3b063ea33574a53e0ebb2a534bb944043ec8b021
Elena2332 <elenafesional@gmail.com>
2022-12-19 13:39:44 +0100

    ahorcado casi terminado
    gestionar fin de partida

---
+++ eclipse/Tema6_Ejercicio0/src/main/java/servlets/ServletComprobar.java
@@ -0,0 +1,107 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class ServletComprobar
+ */
+@WebServlet("/ServletComprobar")
+public class ServletComprobar extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletComprobar() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		comprobar(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	// devuelve la URL ("http://localhost:8080/Tema6_Ejercicio0/") hay que concatenarle el servlet 
+    private static String baseUrl(HttpServletRequest req)
+    {
+        StringBuffer url = req.getRequestURL();
+        String uri = req.getRequestURI();
+        String context = req.getContextPath();
+        return url.substring(0, url.length() - uri.length() + context.length()) + "/";
+    }
+	
+	
+    
+    
+	public void comprobar(HttpServletRequest request, HttpServletResponse response)
+	{
+		HttpSession session = request.getSession(true);  //retoma la sesion
+		
+		if(request.getParameter("btnComprobar") != null)  // comprobar respuesta
+    	{
+    		String res = request.getParameter("inpRes");
+    		if(res != null && res.trim().length()>0)  // si la respuesta no esta vacia
+    		{
+    			session.setAttribute("final", null);  // el juego aun no termina
+    			
+    			if(res.equals(session.getAttribute("palabra")))  // acierta la palabra  termina el juego
+    			{
+    				session.setAttribute("final", true);  //gana
+    			}
+    			else  //falla la palabra
+    			{
+    				session.setAttribute("vidas", ((int)session.getAttribute("vidas")-1) );
+    				if((int)session.getAttribute("vidas") == 0)  // termina el juego
+    				{
+        				session.setAttribute("final", false);  //pierde
+    					
+    				}
+    			}
+    		}
+    	}
+		
+		// revelar letra
+		Integer get = Integer.parseInt(request.getParameter("pos"));
+    	if(get != null)
+    	{
+    		ArrayList<Integer> revelados = (ArrayList) session.getAttribute("revelados");
+    		if(!revelados.contains(get))
+    		{
+				session.setAttribute("vidas", ((int)session.getAttribute("vidas")-1) );
+				revelados.add(get);
+				session.setAttribute("revelados", revelados);
+    		}
+    	}
+        
+    	
+    	try {
+			response.sendRedirect(baseUrl(request)+"ServletJuego");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
--- eclipse/Tema6_Ejercicio0/src/main/java/servlets/ServletJuego.java
+++ eclipse/Tema6_Ejercicio0/src/main/java/servlets/ServletJuego.java
@@ -4,12 +4,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import beans.AlamacenPalabras;
 
@@ -20,17 +20,12 @@ import beans.AlamacenPalabras;
 @WebServlet(name="ServletJuego", urlPatterns = "/ServletJuego")
 public class ServletJuego extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private int vidas;
-	private String palabra;
-	private ArrayList<Integer> revelados;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
     public ServletJuego() {
         super();
-        palabra = AlamacenPalabras.getPalabra();
-        vidas = Math.round(palabra.length()/2);
     }
 
 	/**
@@ -52,7 +47,7 @@ public class ServletJuego extends HttpServlet {
 
 	
 	
-	// devuelve la URL ("http://localhost:8080/Tema6_Ejercicio1/") hay que concatenarle el servlet 
+	// devuelve la URL ("http://localhost:8080/Tema6_Ejercicio0/") hay que concatenarle el servlet 
     private static String baseUrl(HttpServletRequest req)
     {
         StringBuffer url = req.getRequestURL();
@@ -68,24 +63,51 @@ public class ServletJuego extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");        
         try (PrintWriter out = response.getWriter())   // out permite escribir el html
         {
-        	dibujar(request, response, out);  //dibujar la pantalla
+            HttpSession session = request.getSession(false);  // false: si no esta creada la crea y devuelve null
+            if(session == null) //no habia session creada 
+            {
+            	session = request.getSession(true);  // crea la sesion si no esta creada
+                String palabra = AlamacenPalabras.getPalabra().toUpperCase();
+                int vidas = Math.round(palabra.length()/2);
+            	ArrayList<Integer> revelados = new ArrayList<Integer>();
+            	session.setAttribute("palabra", palabra);
+            	session.setAttribute("vidas", vidas);
+            	session.setAttribute("revelados", revelados);            	
+            }
+            
+            
+            dibujar(request, response, out, session);  //dibujar la pantalla
         }
+        catch (Exception e) {
+			// TODO: handle exception
+        	System.out.println(e.toString());
+		}
     }
     
     
 	// crea el html de la pagina
-	public void dibujar(HttpServletRequest request, HttpServletResponse response, PrintWriter out)
+	public void dibujar(HttpServletRequest request, HttpServletResponse response, PrintWriter out, HttpSession session)
 	{	
+		String palabra = (String) session.getAttribute("palabra");
+		int vidas = (Integer) session.getAttribute("vidas");
+		ArrayList<Integer> revelados = (ArrayList) session.getAttribute("revelados");
+		
 		out.print("<!DOCTYPE html> <html> <head><title>Ahorcado xD</title></head> <body>");
 		
 		//formulario
-		out.print("<form action='"+ baseUrl(request) + "ServletJuego' method='post'>");
+		String url = baseUrl(request);
+		out.print("<form action='"+ url + "ServletComprobar' method='post'>");
 		out.print("<table><tr>");
 		for(int i=0; i<palabra.length() ;i++)
 		{
-			out.print("<td><a href='"+ baseUrl(request) + "ServletJuego?pos="+i+"'>Ver</a><td>");
+        	if(revelados.contains(i))
+        		out.print("<td>"+palabra.charAt(i)+"<td>");
+        	else
+        		out.print("<td><a href='"+ url + "ServletComprobar?pos="+i+"'>Ver</a><td>");
 		}
-		
+		out.print("</tr></table>");
+		out.print("<p>- "+vidas+" vidas restantes</p>");
+		out.print("<p>- Tu respuesta <input type='text' name='inpRes'/><button type='submit' name='btnComprobar'>Comprobar</button></p>");
 		out.print("</form>");
 		
 	}
